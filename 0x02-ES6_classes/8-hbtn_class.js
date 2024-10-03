d13c7b8ba0811bb9f60bd00557dc5abd5899cf89
Chimaobi Ubani <wiztherealist@gmail.com>
2024-10-03 20:14:31 +0100

    js

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,33 @@
+export default class HolbertonClass {
+    constructor(size, ) {
+      this.size = size;
+      this.location = location;
+    }
+  
+    /**
+    * @param {String} size
+    */
+    set size(size) {
+      this._size = size;
+    }
+  
+    get size() {
+      return this._size;
+    }
+  
+    /**
+     * @param {Number} location
+     */
+    set location(location) {
+      this._location = location;
+    }
+  
+    get location() {
+      return this._location;
+    }
+  
+    [Symbol.toPrimitive](hint) {
+      if (hint === 'number') return this._location
+      if (hint === 'string') return this._size
+    }
+  }
